b149950f0d3b81df933ff55c436e76c4f885501f
KuoTzu-yang <nmakerbsiwill@gmail.com>
2019-01-28 21:25:24 +0800

    remove test.py from git repo

--- test.py
+++
@@ -1,6 +0,0 @@
-import source_mut_model_generators, model_mut_model_generators
-
-# SMM_generators = source_mut_model_generators.SourceMutatedModelGenerators()
-# SMM_generators.integration_test(verbose=False, with_checkpoint=False)
-
-MMM_generators = model_mut_model_generators.ModelMutatedModelGenerators()
